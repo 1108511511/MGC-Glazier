4bb9f4cd19ddab11a68700143d30f5a84a8cf42d
Donovan Crichton <donovan.crichton@hushmail.com>
2014-06-14 09:02:10 +1000

    Update repo to only include source files

---
+++ Query.java
@@ -0,0 +1,64 @@
+package mgcproject;
+
+import java.sql.*;
+
+/**
+* @author Peter C
+*/
+
+public class Query {
+    private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    private static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/my_test";
+//*** Change to actual DB name ***
+    private static final String USER = "root";
+    private static final String PASS = "";
+    
+    public static void printFromTable(String sqlString) throws SQLException {
+        try {
+            Connection conn = getConnection();
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sqlString);
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int columnsNum = rsmd.getColumnCount();
+            System.out.println("Reading from database");
+            while(rs.next()) {
+                for(int i = 1; i <= columnsNum; i++) {
+                    String columnValue = rs.getString(i);
+                    System.out.println(rsmd.getColumnName(i) + ": " +
+columnValue);
+                }
+            }
+            rs.close();
+        } catch(SQLException e) {
+            System.err.println("Read SQLException: " + e.getMessage());
+        }
+    }
+    
+    public static void writeToTable(String sqlString) throws SQLException {
+        try {
+            Connection conn = getConnection();
+            Statement stmt = conn.createStatement();
+            stmt.executeUpdate(sqlString);
+            System.out.println("Statement written to database");
+        } catch(SQLException e) {
+            System.err.println("Write SQLException: " + e.getMessage());
+        }
+    }
+    
+    public static Connection getConnection() {
+        Connection conn = null;
+        try {
+            try { //register JDBC
+                Class.forName(JDBC_DRIVER);
+            } catch(ClassNotFoundException e) {
+                System.err.println("ClassNotFoundException: " + e.getMessage());
+            }
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            System.out.println("Connected to database");
+        } catch(SQLException e) {
+            System.err.println("Write SQLException: " + e.getMessage());
+        }
+        return conn;
+    }
+    
+}
