7f3789a172e0088eef28a6c630f552cd785f52a7
PeterAC <petercornell@me.com>
2014-06-15 12:25:09 +1000

    complete readFromTable method

--- mgcproject/Query.java
+++ mgcproject/Query.java
@@ -1,17 +1,19 @@
+//testing netbeans git integration
 package mgcproject;
 
 import java.sql.*;
+import com.sun.rowset.CachedRowSetImpl;
+import javax.sql.rowset.CachedRowSet;
 
 /**
-* @author Peter C
-*/
+ * @author Peter C
+ */
 
 public class Query {
     private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    private static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/my_test";
-//*** Change to actual DB name ***
+    private static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/my_test"; //*** Change to actual DB name ***
     private static final String USER = "root";
-    private static final String PASS = "";
+    private static final String PASS = "";  
     
     public static void printFromTable(String sqlString) throws SQLException {
         try {
@@ -24,14 +26,30 @@ public class Query {
             while(rs.next()) {
                 for(int i = 1; i <= columnsNum; i++) {
                     String columnValue = rs.getString(i);
-                    System.out.println(rsmd.getColumnName(i) + ": " +
-columnValue);
+                    System.out.println(rsmd.getColumnName(i) + ": " + columnValue);
                 }
             }
             rs.close();
+            stmt.close();
         } catch(SQLException e) {
             System.err.println("Read SQLException: " + e.getMessage());
         }
+    }
+    
+    public static CachedRowSet readFromTable(String sqlString) throws SQLException {
+        CachedRowSet crs = new CachedRowSetImpl();
+        try {
+            Connection conn = getConnection();
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sqlString);            
+            System.out.println("Reading from database"); 
+            crs.populate(rs);
+            rs.close();
+            stmt.close();
+        } catch(SQLException e) {
+            System.err.println("Read SQLException: " + e.getMessage());
+        }
+        return crs;
     }
     
     public static void writeToTable(String sqlString) throws SQLException {
@@ -42,17 +60,17 @@ columnValue);
             System.out.println("Statement written to database");
         } catch(SQLException e) {
             System.err.println("Write SQLException: " + e.getMessage());
-        }
+        } 
     }
     
     public static Connection getConnection() {
         Connection conn = null;
         try {
-            try { //register JDBC
-                Class.forName(JDBC_DRIVER);
+            try {   //register JDBC
+                Class.forName(JDBC_DRIVER);     
             } catch(ClassNotFoundException e) {
                 System.err.println("ClassNotFoundException: " + e.getMessage());
-            }
+            }   
             conn = DriverManager.getConnection(DB_URL, USER, PASS);
             System.out.println("Connected to database");
         } catch(SQLException e) {
