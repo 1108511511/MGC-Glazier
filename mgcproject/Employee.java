a196b09ca3ff24d0fee59fcc70a1708b473878da
unknown <3106909413@CCI-12950.tafe.mst>
2014-06-19 15:58:34 +1000

    finished Employee class etc.

--- mgcproject/Employee.java
+++ mgcproject/Employee.java
@@ -6,21 +6,44 @@
 
 package mgcproject;
 
+import com.sun.rowset.CachedRowSetImpl;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.rowset.CachedRowSet;
+
 /**
  *
  * @author 1108511511
  */
-public class Employee extends Person{
+public class Employee extends Person {
     private int employeeId;
-    private enum employeeRole{Glazier, Sales, Manager};
+    private String employeeRole;
+    private String password;
     
-    Employee(){
+    Employee() {
         super();
         employeeId = 0;
     }
-    Employee(int employID, String fName, String lName, enum employeeRole{Glazier, Sales, Manager} ){
-    super(fName,lName);
-    this.employeeId = employID;
+    
+    Employee(String firstName, String lastName, String employeeRole, String password) {
+        super(firstName,lastName);
+        this.employeeId = getEmployeeIdFromDB() + 1;
+        this.employeeRole = employeeRole;
+        this.password = password;
+    }
+    
+    private int getEmployeeIdFromDB() {
+        try {
+            CachedRowSet crs = new CachedRowSetImpl();
+            crs = Query.readFromTable(SQLStatements.selectMaxEmployeeIdStmt()); 
+            crs.next();
+            int maxId = crs.getInt("employee_id");
+            return maxId;
+        } catch (SQLException e) {
+            System.err.println("Error: " + e.getMessage());
+            return -1;
+        }
     }
 
     /**
@@ -37,16 +60,13 @@ public class Employee extends Person{
         this.employeeId = employeeId;
     }
     
-    
-        @Override
-        public List getFields(){
+    @Override
+    public List getFields(){
         List<Object> list = new ArrayList<Object>();
         list = super.getFields();
         list.add(employeeRole);
         list.add(employeeId);
-        return list;
-        
-                
+        return list;          
     }
     
 }
--- mgcproject/MGCProject.java
+++ mgcproject/MGCProject.java
@@ -12,8 +12,7 @@ public class MGCProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {          
-        Product p = new Product("Tough", true, true, 12, 23, 3, 1);
-        System.out.println(p.getUnitPrice());
-        System.out.println(p.getProductPrice());
+        Employee e = new Employee("Pete", "Cornell", "manager", "password");
+        System.out.println(e.getEmployeeId());
     }
 }
