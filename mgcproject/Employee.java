2e1fd688c8e8de771639aef2ea2c34c4af3aef32
Daniel Bassett <dkbassett@hotmail.com>
2014-06-23 08:24:33 +1000

    more updates from Friday

---
+++ mgcproject/Employee.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package myperson;
+
+/**
+ *
+ * @author 1108511511
+ */
+public class Employee extends Person{
+    private int employeeId;
+    private enum employeeRole{Glazier, Sales, Manager};
+    
+    Employee(){
+        super();
+        employeeId = 0;
+    }
+    Employee(int employID, String fName, String lName, enum employeeRole{Glazier, Sales, Manager} ){
+    super(fName,lName);
+    this.employeeId = employID;
+    }
+
+    /**
+     * @return the employeeId
+     */
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    /**
+     * @param employeeId the employeeId to set
+     */
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+    
+    
+        @Override
+        public List getFields(){
+        List<Object> list = new ArrayList<Object>();
+        list = super.getFields();
+        list.add(employeeRole);
+        list.add(employeeId);
+        return list;
+        
+                
+    }
+    
+}
